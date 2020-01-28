b700815a9f156930e2774720d64e741956d68d23
messyoxd <messyo@hotmail.com>
2020-01-28 17:18:32 -0300

    primeirasso commit e esse monodevelop crasha demais

---
+++ Teste2/Teste2/MainWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Gtk;
+using Teste2;
+
+public partial class MainWindow : Gtk.Window
+{
+    WatchClock wc;
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        drawingArea1.ModifyBg(StateType.Normal, new Gdk.Color(5, 5, 5));
+        wc = new WatchClock();
+        ClockStart();
+    }
+
+    void ClockStart()
+    {
+        GLib.Timeout.Add(100, new GLib.TimeoutHandler(Update));
+    }
+
+    bool Update()
+    {
+        drawingArea1.GdkWindow.Clear();
+        TimeStamp.Text = DateTime.Now.ToString("HH:mm:ss");
+        wc.DrawRing(drawingArea1.GdkWindow);
+        wc.HourLine(drawingArea1.GdkWindow);
+        wc.MinuteLine(drawingArea1.GdkWindow);
+        wc.SecondLine(drawingArea1.GdkWindow);
+
+        return true;
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
