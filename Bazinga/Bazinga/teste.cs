57a3fdcd7f0c53f353a4d60ff513616c011ca747
Messyoxd <messyo@hotmail.com>
2020-01-30 05:55:05 -0300

    testando udp

---
+++ Bazinga/Bazinga/teste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Bazinga
+{
+    public partial class teste : Gtk.Window
+    {
+        public teste() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
---
+++ TesteBroadcastUPDReceiver/TesteBroadcastUPDReceiver/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace TesteBroadcastUPDReceiver
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Socket sockBroadcastReceiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipepLocal = new IPEndPoint(IPAddress.Any, 23000);
+
+            byte[] receiverBuffer = new byte[512];
+            int nCountReceived = 0;
+            try
+            {
+                sockBroadcastReceiver.Bind(ipepLocal);
+                while (true)
+                {
+                    nCountReceived += sockBroadcastReceiver.Receive(receiverBuffer);
+                    Console.WriteLine("Total number of bytes received: " + nCountReceived);
+                    Array.Clear(receiverBuffer,0,receiverBuffer.Length);
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
